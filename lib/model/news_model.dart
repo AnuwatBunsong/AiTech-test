df6fe47e02f45e8e23b12645f14d965ec5ba89b3
Thanawat <Thanawat.t@happyproducts.co.th>
2020-10-26 17:54:41 +0700

    commit

--- lib/model/news_model.dart
+++ lib/model/news_model.dart
@@ -14,13 +14,15 @@ class News {
   final String title;
   final String shortDescription;
   final String date;
+  final String thumbnail;
 
-  const News({this.title, this.shortDescription, this.date});
+  const News({this.title, this.shortDescription, this.date, this.thumbnail});
 
   News.fromMap(Map<String, dynamic> map)
       : title = map['title'],
         shortDescription = map['short_description'],
-        date = map['created_at'];
+        date = map['created_at'],
+        thumbnail = map['image']['thumbnail_url'];
 }
 
 abstract class NewsRepository {
