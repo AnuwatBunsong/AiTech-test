1ab22d1978ea5c5b3a63d6b906b860be1324c9a1
DESKTOP-ETKJE2C\Killer <six.heart1773@gmail.com>
2020-12-10 16:12:40 +0700

    commit

--- lib/utils/helper.dart
+++ lib/utils/helper.dart
@@ -1,5 +1,18 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 class DeviceWidth {
   static Size size;
+}
+
+// ignore: non_constant_identifier_names
+GetFileSize(String absolutePathImage) {
+  return File(absolutePathImage).lengthSync();
+}
+
+// ignore: non_constant_identifier_names
+GetFileName(String absolutePathImage) {
+  File file = new File(absolutePathImage);
+  return basename(file.path);
 }
--- lib/view/payment.dart
+++ lib/view/payment.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:cremation/utils/widget.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:cremation/utils/helper.dart';
 
 class PaymentPage extends StatefulWidget {
   @override
@@ -14,6 +16,7 @@ class _PaymentPageState extends State<PaymentPage> {
   final picker = ImagePicker();
   StreamController _stream;
   List _imageList = [];
+  int id = 0;
 
   @override
   initState() {
@@ -26,7 +29,17 @@ class _PaymentPageState extends State<PaymentPage> {
 
     setState(() {
       if (pickedFile != null) {
+        id++;
         _image = File(pickedFile.path);
+        var object = {
+          'id' : id,
+          'image': pickedFile.path.toString(),
+          'name': GetFileName(pickedFile.path),
+          'size': GetFileSize(pickedFile.path)
+        };
+
+        _imageList.add(object);
+        _stream.add(_imageList);
       } else {
         print('No image selected.');
       }
@@ -35,23 +48,61 @@ class _PaymentPageState extends State<PaymentPage> {
 
   Future getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.gallery);
-    final bytes = await pickedFile.readAsBytes();
 
     setState(() {
       if (pickedFile != null) {
+        id++;
         _image = File(pickedFile.path);
         var object = {
+          'id' : id,
           'image': pickedFile.path.toString(),
-          'size': _image.readAsBytes()
+          'name': GetFileName(pickedFile.path),
+          'size': GetFileSize(pickedFile.path)
         };
-        print(_image.readAsBytes());
+
         _imageList.add(object);
         _stream.add(_imageList);
-        print(_stream);
       } else {
         print('No image selected.');
       }
     });
+  }
+
+  void _deleteImage(int id) {
+    setState(() {
+      _imageList.removeWhere((item) => id == id);
+      _stream.add(_imageList);
+    });
+  }
+
+  void _submit() {
+    Alert(
+      context: context,
+      type: AlertType.success,
+      title: "อัพโหลดข้อมูลสำเร็จ",
+      style: AlertStyle(
+          titleStyle: TextStyle(
+              color: Colors.green, fontSize: 20, fontFamily: 'SukhumvitText')),
+      buttons: [
+        DialogButton(
+          child: Text(
+            "ตกลง",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontFamily: 'SukhumvitText'),
+          ),
+          onPressed: () => Navigator.pushNamed(context, '/login'),
+          width: 120,
+        )
+      ],
+    ).show();
+  }
+  
+
+  @override
+  void dispose() {
+    _imageList = [];
+    _stream.close();
+    super.dispose();
   }
 
   @override
@@ -185,93 +236,30 @@ class _PaymentPageState extends State<PaymentPage> {
                 StreamBuilder(
                   stream: _stream.stream,
                   builder: (BuildContext context, snapshot) {
-                    print(snapshot);
                     if (snapshot.hasError)
                       return new Text('Error: ${snapshot.error}');
-                    if (snapshot.data == null) {
+                    if (snapshot.data == null || snapshot.data.isEmpty ) {
                       //return CircularProgressIndicator();
                       return Text('No image selected.');
                     }
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return const Text('Loading...');
-                        break;
-                      case ConnectionState.active:
-                        return const Text('New Image...');
-                        break;
-                      default:
-                        return Text('No image selected.');
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, index) {
+                          return _imageItem(snapshot.data[index]);
+                        },
+                      );
                     }
                   },
                 ),
-                Container(
-                    padding: EdgeInsets.only(left: 16, right: 7),
-                    child: Column(children: [
-                      Stack(children: [
-                        Container(
-                            padding: EdgeInsets.only(top: 10, right: 9),
-                            margin: EdgeInsets.only(bottom: 10),
-                            child: Card(
-                                elevation: 2,
-                                child: Container(
-                                    padding: EdgeInsets.all(13),
-                                    child: Container(
-                                        child: Row(children: [
-                                      Container(
-                                          width: 50,
-                                          height: 50,
-                                          margin: EdgeInsets.only(right: 10),
-                                          decoration: new BoxDecoration(
-                                            color: Color(0xFFE9D9CE),
-                                            shape: BoxShape.circle,
-                                          ),
-                                          child: Icon(Icons.camera_alt,
-                                              size: 20,
-                                              color: Color(0xFFFFFFFF))),
-                                      Expanded(
-                                          child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              mainAxisSize: MainAxisSize.max,
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.start,
-                                              children: [
-                                            Text('Pic.jpg',
-                                                style: TextStyle(
-                                                    color: Color(0xFF000000),
-                                                    fontFamily: 'SukhumvitText',
-                                                    fontSize: 18,
-                                                    fontWeight:
-                                                        FontWeight.w500)),
-                                            Text('124 kb',
-                                                style: TextStyle(
-                                                    color: Color(0xFFDADADA),
-                                                    fontFamily: 'SukhumvitText',
-                                                    fontSize: 16,
-                                                    fontWeight:
-                                                        FontWeight.w500))
-                                          ])),
-                                      Container(
-                                          child: Icon(Icons.check,
-                                              size: 30,
-                                              color: Color(0xFF27AE60)))
-                                    ]))))),
-                        Positioned(
-                            top: 0,
-                            right: 0,
-                            child: Container(
-                                color: Color(0xFFFFFFFF),
-                                child: Icon(Icons.cancel_outlined,
-                                    size: 30, color: Color(0xFFEDEDED))))
-                      ])
-                    ]))
                 /*new Flex(
                   direction: Axis.vertical,
                   children: <Widget>[Image.asset('assets/images/mockup1.png')],
                 ),*/
               ]))
         ]),
-        bottomNavigationBar: Container(
+        bottomNavigationBar: (_imageList.length > 0) ? Container(
             height: 50.0,
             margin: const EdgeInsets.only(bottom: 15.0),
             padding: EdgeInsets.symmetric(horizontal: 25),
@@ -291,12 +279,85 @@ class _PaymentPageState extends State<PaymentPage> {
                     child: Container(
                         constraints: BoxConstraints(minHeight: 50.0),
                         alignment: Alignment.center,
+                        child: GestureDetector(
+                                    onTap: () {
+                                      _submit();
+                                    },
                         child: Text("อัพโหลด",
                             textAlign: TextAlign.center,
                             style: TextStyle(
                               color: Color(0xFFFFFFFF),
                               fontFamily: 'SukhumvitText',
                               fontSize: 20,
-                            )))))));
+                            ))))))): null);
+  }
+
+  Widget _imageItem(item) {
+    return Container(
+      padding: EdgeInsets.only(left: 16, right: 7),
+      child: Column(children: [
+        Stack(children: [
+          Container(
+              padding: EdgeInsets.only(top: 10, right: 9),
+              margin: EdgeInsets.only(bottom: 10),
+              child: Card(
+                  elevation: 2,
+                  child: Container(
+                      padding: EdgeInsets.all(13),
+                      child: Container(
+                          child: Row(children: [
+                        Container(
+                            width: 50,
+                            height: 50,
+                            margin: EdgeInsets.only(right: 10),
+                            decoration: new BoxDecoration(
+                              color: Color(0xFFE9D9CE),
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(Icons.camera_alt,
+                                size: 20,
+                                color: Color(0xFFFFFFFF))),
+                        Expanded(
+                            child: Column(
+                                crossAxisAlignment:
+                                    CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.start,
+                                children: [
+                              Text(item['name'],
+                                  style: TextStyle(
+                                      color: Color(0xFF000000),
+                                      fontFamily: 'SukhumvitText',
+                                      fontSize: 18,
+                                      fontWeight:
+                                          FontWeight.w500)),
+                              Text(item['size'].toString() + ' byte',
+                                  style: TextStyle(
+                                      color: Color(0xFFDADADA),
+                                      fontFamily: 'SukhumvitText',
+                                      fontSize: 16,
+                                      fontWeight:
+                                          FontWeight.w500))
+                            ])),
+                        Container(
+                            child: Icon(Icons.check,
+                                size: 30,
+                                color: Color(0xFF27AE60)))
+                      ]))))),
+          Positioned(
+              top: 0,
+              right: 0,
+              child: GestureDetector(
+                    onTap: () {
+                      _deleteImage(item['id']);
+                    },
+              child: Container(
+                  
+                  color: Color(0xFFFFFFFF),
+                  child: Icon(Icons.cancel_outlined,
+                      size: 30, color: Color(0xFFEDEDED)))))
+        ])
+      ]));
   }
 }
