6d857d4cef222ef4489c6e1ea7140f191e41e76a
Thanawat <Thanawat.t@happyproducts.co.th>
2020-12-16 20:04:19 +0700

    commit

--- lib/presenter/firebase_messaging_presenter.dart
+++ lib/presenter/firebase_messaging_presenter.dart
@@ -1,23 +1,40 @@
+import 'dart:async';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationService {
-  final FirebaseMessaging _fcm;
+  final FirebaseMessaging _firebaseMessaging;
 
-  PushNotificationService(this._fcm);
+  PushNotificationService(this._firebaseMessaging);
+
+  static Future<dynamic> myBackgroundMessageHandler(
+      Map<String, dynamic> message) {
+    if (message.containsKey('data')) {
+      // Handle data message
+      final dynamic data = message['data'];
+    }
+
+    if (message.containsKey('notification')) {
+      // Handle notification message
+      final dynamic notification = message['notification'];
+    }
+
+    // Or do other work.
+  }
 
   Future initialise() async {
     /*if (Platform.isIOS) {
       _fcm.requestNotificationPermissions(IosNotificationSettings());
     }*/
 
-    String token = await _fcm.getToken();
+    String token = await _firebaseMessaging.getToken();
     //print("FirebaseMessaging token: $token");
 
-    _fcm.configure(
+    _firebaseMessaging.configure(
       onMessage: (Map<String, dynamic> message) async {
         print("onMessage: $message");
         return;
       },
+      /*onBackgroundMessage: myBackgroundMessageHandler,*/
       onLaunch: (Map<String, dynamic> message) async {
         print("onLaunch: $message");
         return;
@@ -26,6 +43,7 @@ class PushNotificationService {
         print("onResume: $message");
         return;
       },
+      /*onBackgroundMessage: myBackgroundMessageHandler*/
     );
   }
 }
